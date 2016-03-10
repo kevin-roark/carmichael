f38528642e05ef413bb0bb1d7ad048e4347fc111
Kevin Roark <ker2143@columbia.edu>
2016-03-10 12:39:52 -0500

    true youtube links in /rebourne

--- rebourne/rebourne.js
+++ rebourne/rebourne.js
@@ -2,7 +2,7 @@
 (function() {
   var links = document.querySelectorAll('.links li a');
 
-  var colors = ['rgb(0, 18, 255)', 'rgb(255, 0, 138)', 'rgb(0, 221, 141)', 'rgb(120, 0, 255)', 'rgb(255, 92, 0)'].sort(function() { return Math.random() - 0.5; });
+  var colors = ['rgb(0, 18, 255)', 'rgb(255, 0, 138)', 'rgb(0, 221, 141)', 'rgb(120, 0, 255)', 'rgb(255, 92, 0)', 'rgb(255, 48, 3)', 'rgb(255, 230, 3)'].sort(function() { return Math.random() - 0.5; });
 
   var intro = document.querySelector('.intro');
   var introBottom = intro.getBoundingClientRect().bottom + 10 || 120;
@@ -10,7 +10,9 @@
   var yOffset = introBottom;
   for (var i = 0; i < links.length; i++) {
     var link = links[i];
-    link.style.color = colors[i];
+
+    link.style.color = colors[i % colors.length];
+
     link.style.position = 'absolute';
     link.style.top = yOffset + 'px';
 
@@ -24,4 +26,6 @@
 
     yOffset += (Math.random() * 30 + 90);
   }
+
+  document.body.style.height = (yOffset + 300) + 'px';
 })();
