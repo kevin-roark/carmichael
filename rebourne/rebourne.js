e9c57c61a9e08f7bdaeb3c725edd49cbbe243353
Kevin Roark <ker2143@columbia.edu>
2016-03-10 11:29:38 -0500

    /rebourne

---
+++ rebourne/rebourne.js
@@ -0,0 +1,27 @@
+
+(function() {
+  var links = document.querySelectorAll('.links li a');
+
+  var colors = ['rgb(0, 18, 255)', 'rgb(255, 0, 138)', 'rgb(0, 221, 141)', 'rgb(120, 0, 255)', 'rgb(255, 92, 0)'].sort(function() { return Math.random() - 0.5; });
+
+  var intro = document.querySelector('.intro');
+  var introBottom = intro.getBoundingClientRect().bottom + 10 || 120;
+
+  var yOffset = introBottom;
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    link.style.color = colors[i];
+    link.style.position = 'absolute';
+    link.style.top = yOffset + 'px';
+
+    var x = Math.random() * 60 + 20;
+    if (i % 2 === 0) {
+      link.style.left = x + 'px';
+    }
+    else {
+      link.style.right = x + 'px';
+    }
+
+    yOffset += (Math.random() * 30 + 90);
+  }
+})();
