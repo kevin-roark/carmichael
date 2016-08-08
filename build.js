622d512c7e56ac8b761d428827d9365652e347da
Kevin Roark <ker2143@columbia.edu>
2016-08-08 14:49:43 -0400

    Featured section

--- build.js
+++ build.js
@@ -6,15 +6,15 @@ var beautify = require('js-beautify');
 var contents = require('./content.json');
 
 fs.readFile('base.html', 'utf8', function(err, base) {
-  var contentContainer = '<div class="content-container">';
-  var containerLocation = base.indexOf(contentContainer) + contentContainer.length;
+  var featuredContainer = '<div class="content-container featured-container">';
+  var featuredLocation = base.indexOf(featuredContainer) + featuredContainer.length;
+  var featured = `\n` + contents.featured.map(renderContent).join('\n');
 
-  var renderedContent = '\n';
-  contents.forEach(function(content) {
-    renderedContent += renderContent(content) + '\n';
-  });
+  var otherContainer = '<div class="content-container other-container">';
+  var otherLocation = base.indexOf(otherContainer) + otherContainer.length;
+  var other = `\n` + contents.other.map(renderContent).join('\n');
 
-  var indexHTML = `${base.substring(0, containerLocation)}${renderedContent}${base.substring(containerLocation + 1)}`;
+  var indexHTML = `${base.substring(0, featuredLocation)}${featured}${base.substring(featuredLocation + 1, otherLocation)}${other}${base.substring(otherLocation + 1)}`;
   indexHTML = beautify.html(indexHTML, {
     indent_size: 2
   });
